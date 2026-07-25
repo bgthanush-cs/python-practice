dc01a91a57ef9531a14c51aec5a45958149271c4
Thanush B G <bgthanush@gmail.com>
2026-07-25 16:31:30 +0530

    Day 3: Learned Python Functions

---
+++ day3-functions.py
@@ -0,0 +1,50 @@
+# Day 3 - Functions Practice
+
+# Function 1
+def introduce():
+    print("My name is Thanush")
+    print("I am learning Python")
+    print("I will become a great programmer")
+
+introduce()
+
+
+# Function 2
+def morning():
+    print("Good morning")
+    print("Have a nice day")
+
+morning()
+morning()
+morning()
+
+
+# Function with one parameter
+def greet(name):
+    print("Welcome", name)
+
+greet("Thanush")
+greet("Rahul")
+greet("Karthik")
+
+
+# Function with age parameter
+def age(year):
+    print("Your age is", year)
+
+age(17)
+
+
+# Function to find square
+def square(num):
+    print("The square is", num * num)
+
+square(5)
+
+
+# Function with two parameters
+def student(name, age):
+    print("Name:", name)
+    print("Age:", age)
+
+student("Thanush", 17)
