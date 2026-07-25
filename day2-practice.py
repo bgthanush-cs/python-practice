a34443cc925173710765ec5643b8f50367d84b36
Thanush B G <bgthanush@gmail.com>
2026-07-25 16:33:51 +0530

    Day 2: Python Basics Practice
    - Practiced user input
    - Learned if-else statements
    - Practiced for loops
    - Practiced while loops

---
+++ day2-practice.py
@@ -0,0 +1,18 @@
+# If-Else Program
+name = input("What is your name?")
+
+if name == "Thanush":
+    print("Welcome", name)
+else:
+    print("Hello", name)    
+
+#For Loop Program
+for i in range(1,6):
+    print(i)
+
+#While Loop Program
+x = 1
+
+while x <= 5:
+    print(x)
+    x = x + 1
