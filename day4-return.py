377ea443b46235d4aa9d98527c42b1fcc97f9218
Thanush B G <bgthanush@gmail.com>
2026-07-26 16:12:42 +0530

    Day 4: Return Values in Python
    - Learned the return keyword
    - Understood the difference between print() and return
    - Practiced functions that return values

---
+++ day4-return.py
@@ -0,0 +1,17 @@
+# Day 4 - Return Practice
+
+def square(num):
+    return num*num
+
+print(square(7))
+
+def add(a, b):
+    return a + b
+
+print(add(10, 20))
+
+def fullname(first, last):
+    return first + " " + last
+
+print(fullname("Thanush", "B.G"))
+
