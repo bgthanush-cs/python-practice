1a0c83b5f18cde231797e91a9607afb912038bd2
Thanush B G <bgthanush@gmail.com>
2026-07-28 16:45:43 +0530

    Day 6: Learned Python Dictionaries
    Practiced creating dictionaries, accessing values, updating existing keys, and adding new key-value pairs.

---
+++ day6-dictionaries.py
@@ -0,0 +1,16 @@
+student = {
+    "Name" : "Thanush" ,
+    "Age" : "17" ,
+    "Course" : "CSE"    
+}
+
+print(student["Name"])
+
+student["college"] = "ABC College"
+
+print(student)
+
+Phone = {
+    "Brand" : "Motorola" ,
+    "Model" : "G86"
+}
