65dcdf8df42d06458de74ccba7baad45c57b0ca9
Thanush B G <bgthanush@gmail.com>
2026-07-28 16:48:11 +0530

    Day 6: Learned Python range() Function
    Practiced the range() function with different start, stop, and step values, including reverse counting.

---
+++ day6-range.py
@@ -0,0 +1,13 @@
+#range(start, stop)
+for i in range(1, 11):
+    print(i)
+
+#range(start, stop, step
+for i in range( 2, 21, 2):
+    print(i)
+
+#range(number)
+for i in range(5):
+    print("Thanush")
+    
+    
