2ece3d60eec2483c3d433129a17f0666b9f6fb3b
Thanush B G <bgthanush@gmail.com>
2026-07-29 19:51:51 +0530

    Day 7: Revised Python Dictionaries

---
+++ day7-dictionary-revision.py
@@ -0,0 +1,17 @@
+# Day 7 - Dictionary Revision
+
+student = {
+    "name" : "Thanush" ,
+    "age" : "17"
+}
+
+print(student["name"])
+print(student["age"])
+
+student["age"] = 18
+
+print(student["age"])
+
+student["college"] = "ABC College"
+
+print(student)
