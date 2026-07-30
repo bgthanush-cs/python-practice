708fa1547ab1ae288aeb22ef2ed90c632c3f440e
Thanush B G <bgthanush@gmail.com>
2026-07-30 20:53:38 +0530

    Enhance dictionary loops with phone details
    Added a new dictionary for phone details and printed its contents.

--- day8-dictionary-loops.py
+++ day8-dictionary-loops.py
@@ -1,5 +1,6 @@
 # Dictionary Loops
 
+# Creating a dictionary
 student = {
     "name" : "Thanush",
     "age" : "17",
@@ -7,18 +8,34 @@ student = {
     "branch" : "CSE"
 }
 
+# Printing all keys
 print("Keys:")
 for key in student:
     print(key)
 
 print()
 
+# Printing all values
 print("Values:")
 for key in student:
     print(student[key])
 
 print()
 
+# Printing keys and values together
 print("Key : Value")
 for key in student:
     print(key, ":" , student[key])
+
+# Another dictionary
+phone = {
+    "brand": "Motorola",
+    "model": "Moto G86",
+    "color": "Blue"
+}
+
+print()
+
+# Printing phone details
+for key in phone:
+    print(key, ":", phone[key])
