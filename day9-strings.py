e00248dc26f7792b82ba3023a22b794d7de8279a
Thanush B G <bgthanush@gmail.com>
2026-07-31 19:47:05 +0530

    Day 9: Learned Python Strings
    Practiced string indexing, negative indexing, slicing, len(), upper(), and lower() methods.

---
+++ day9-strings.py
@@ -0,0 +1,43 @@
+ #Day 9 - Python strings
+# Learned :
+# - String indexing 
+# - Negative Indexing
+# - len()
+# - upper()
+# - lower()
+# - String slicing
+
+name = "Thanush"
+
+print("Original string:")
+print(name)
+
+print()
+
+print("Indexing:")
+print(name[0])
+print(name[3])
+print(name[-1])
+
+print()
+
+print("Lemgth:")
+print(len(name))
+
+print()
+
+print("Uppercase:")
+print(name.upper())
+
+print()
+
+print("Lowercase:")
+print(name.lower())
+
+print()
+
+print("string slicing:")
+print(name[0:3])
+print(name[2:5])
+print(name[:4])
+print(name[4:])
