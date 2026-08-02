661ca4596b26dbf5003269e523eea7af112e2564
Thanush B G <bgthanush@gmail.com>
2026-08-02 20:59:20 +0530

    Day 11: Practiced Basic File Handling

---
+++ day11-streak.py
@@ -0,0 +1,12 @@
+# Day 11 - Streak Saver
+# Name: Thanush.B.G
+
+print("Day 11 Complete!")
+
+file = open("streak.txt", "a")
+
+file.write("day 11 Complete\n")
+
+file.close()
+
+print("Streak saved successfully!")
