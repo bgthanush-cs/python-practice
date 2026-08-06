1418b2581646e8ce92b8a713e0a32cf592aec64b
Thanush B G <bgthanush@gmail.com>
2026-08-06 21:21:35 +0530

    Refactor age validation and ID check logic

--- day15-logical-operators.py
+++ day15-logical-operators.py
@@ -2,16 +2,16 @@
 # Name: Thanush.B.G
 
 age = int(input("Enter your age: "))
-has_id = input("Do you have an ID? (yes/no):")
+has_id = input("Do you have an ID? (yes/no): ").lower()
 
-if age >= 18 and has_id == "yes":
+if age <= 0:
+    print("Invalid age!")
+
+elif age >= 18 and has_id == "yes":
     print("Entry Allowed")
 
 elif age >= 18 and has_id == "no":
     print("Bring your ID")
-
-elif age <= 0:
-    print("Invalid age!")
 
 else:
     print("You are underage")
