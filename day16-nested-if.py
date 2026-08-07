ee8faf9301eea2653c677ac197c259e97f2bf2a6
Thanush B G <bgthanush@gmail.com>
2026-08-07 21:44:12 +0530

    Fix formatting and improve output messages

--- day16-nested-if.py
+++ day16-nested-if.py
@@ -3,7 +3,7 @@
 
 age = int(input("Enter your age: "))
 
-if age <= 0:
+if age < 0:
     print("Invalid age!")
 
 elif age >= 18:
@@ -11,13 +11,13 @@ elif age >= 18:
     has_id = input("Do you have an ID? (yes/no): ").lower()
 
     if has_id == "yes":
-        print("Entry Allowed") 
+        print("Entry allowed") 
 
     elif has_id == "no":
         print("Bring your ID")    
 
     else:
-        print("Invalid Choice!")
+        print("Invalid choice!")
 
 else:
     print("You are underage")
