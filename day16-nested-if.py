052e36d9dc20f6dbf61a1967274e4f9bc9bede34
Thanush B G <bgthanush@gmail.com>
2026-08-07 21:30:45 +0530

    Day 16: Learned nested if statements

---
+++ day16-nested-if.py
@@ -0,0 +1,24 @@
+# Day 16 - Nested if
+# Name: Thanush.B.G
+
+age = int(input("Enter your age: "))
+
+if age <= 0:
+    print("Invalid age!")
+
+elif age >= 18:
+
+    has_id = input("Do you have an ID? (yes/no): ").lower()
+
+    if has_id == "yes":
+        print("Entry Allowed") 
+
+    elif has_id == "no":
+        print("Bring your ID")    
+
+    else:
+        print("Invalid Choice!")
+
+else:
+    print("You are underage")
+
