1ebd1bcf427b0b2bf9a35193260f613ff0a6f2c7
Thanush B G <bgthanush@gmail.com>
2026-08-11 19:54:24 +0530

    Day 20: Learned set modification

---
+++ day20-set-modification.py
@@ -0,0 +1,11 @@
+# Day 20 - Set Modification
+# Name: Thanush B.G
+
+games = {"Minecraft","GTA","BGMI"}
+
+games.remove("GTA")
+games.add("Valorant")
+games.discard("Free Fire")
+
+print(games)
+print(len(games))
