dd9a35d64ea9a5b3a372c1902103d84d8503f27f
Thanush B G <bgthanush@gmail.com>
2026-08-12 19:50:43 +0530

    Day 21: Learned f-strings

---
+++ day21-f-strings.py
@@ -0,0 +1,21 @@
+# Day 21 - f-Strings
+# Name: Thanush B.G
+
+name = "Thanush"
+age = 17
+language = "Python"
+
+print(f"My name is {name}.")
+print(f"I am learning {language}.")
+print(f"I am {age} years old.")
+print(f"Next year I will be {age + 1}.")
+
+marks = 85
+
+if marks >= 40:
+    result = "Pass"
+else:
+    result = "Fail" 
+
+print(f"{name} scored {marks} marks and the result is {result}")   
+
