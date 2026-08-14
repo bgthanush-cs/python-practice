eb69c31045c2f7207bfccc514bcca6e901835889
Thanush B G <bgthanush@gmail.com>
2026-08-14 21:03:06 +0530

    Day 23: Practiced logical operators and conditions

---
+++ Day 23-Logical-Operators-and-Conditions.py
@@ -0,0 +1,9 @@
+# Day 23 - Logical Operators and Conditions
+
+age = int(input("Enter your age: "))
+money = int(input("Enter the amount: "))
+
+if age >= 12 and money >= 150:
+    print("You can buy the ticket")
+else:
+    print("Sorry, you cannot buy the ticket")
