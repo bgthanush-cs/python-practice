59c51bd84f8e1fdbaa78b357afe105fd5771bc16
Thanush B G <bgthanush@gmail.com>
2026-08-17 22:09:13 +0530

    Day 26: Largest number comparison

---
+++ day-26-largest_of_two_numbers.py
@@ -0,0 +1,12 @@
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+    
+if num1 > num2:
+    print(f"Largest number = {num1}")
+
+elif num1 < num2:
+    print(f"Largest number = {num2}")
+
+else:
+    print(f"Both numbers are equal")
+    
